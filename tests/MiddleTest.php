a04727ae0d8fb041080a371836f698539039a46e
fabio menezes <fabiomenezes@admedia.pt>
2017-09-04 20:14:46 +0100

    satis

---
+++ tests/MiddleTest.php
@@ -0,0 +1,39 @@
+<?php 
+
+/**
+*  Corresponding Class to test YourClass class
+*
+*  For each class in your library, there should be a corresponding Unit-Test for it
+*  Unit-Tests should be as much as possible independent from other test going on.
+*
+*  @author yourname
+*/
+class MiddleTest extends PHPUnit_Framework_TestCase{
+	
+  /**
+  * Just check if the YourClass has no syntax error 
+  *
+  * This is just a simple check to make sure your library has no syntax error. This helps you troubleshoot
+  * any typo before you even use this library in a real project.
+  *
+  */
+  public function testIsThereAnySyntaxError(){
+	$var = new Tc24\Template\Middle;
+	$this->assertTrue(is_object($var));
+	unset($var);
+  }
+  
+  /**
+  * Just check if the YourClass has no syntax error 
+  *
+  * This is just a simple check to make sure your library has no syntax error. This helps you troubleshoot
+  * any typo before you even use this library in a real project.
+  *
+  */
+  public function testMethod1(){
+	$var = new Tc24\Template\Middle;
+	$this->assertTrue($var->render("hey") === 'Hello World');
+	unset($var);
+  }
+  
+}
